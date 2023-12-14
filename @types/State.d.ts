067a742a0ff9aecff241bbfcb76af5618187ee2b
lippiece <github@lippiece.anonaddy.me>
2023-12-14 16:32:47 +0200

    fix(state & render): re-render on state change

--- @types/State.d.ts
+++
@@ -1,6 +0,0 @@
-import { WritableAtom } from "nanostores"
-
-  export type Recipe<T> = (draft: T) => T
-  export type State<T> = WritableAtom | {
-    set: (recipe: Recipe<T>) => void
-  }
--- main.ts
+++ main.ts
@@ -2,27 +2,20 @@ import { produce } from "immer"
 import { render,TemplateResult  } from "lit"
 import { atom } from "nanostores"
 
-import { Recipe, State } from "./@types/State"
+import { Recipe } from "./@types/State"
 import WebComponentConfiguration from "./@types/WebComponentConfiguration"
 
 /**
  * Creates a web component template based on the provided configuration.
  *
  * The returned class will:
- *     Extend HTMLElement and call super() in constructor
- *     Create shadow root and clone template
- *     Set ARIA attributes in connectedCallback
- *     Remove event listeners in disconnectedCallback
- *     Implement attribute changed callbacks
- *     Reflect attributes to properties
+ *     Extend HTMLElement
  *     Render() accepts defaultState and renders shadow DOM
- *     Properties will be generated from config
- *     Event handlers will be added from config
  *     State initialized from config
  *     Methods can be added by extending class after it is returned
  *
  * @param {WebComponentConfiguration<StateGeneric>} configuration - The configuration
- *   object for the web component.
+ *  object for the web component.
  * @return {WebComponentClass} The new web component class.
  */
 const createWebComponentBaseClass = <StateGeneric>(
@@ -31,7 +24,7 @@ const createWebComponentBaseClass = <StateGeneric>(
   const stateAtom = atom<StateGeneric>(configuration.defaultState)
 
   return class WebComponent extends HTMLElement {
-    state: State<StateGeneric> = {
+    state = {
       ...stateAtom,
 
       set: (recipe: Recipe<StateGeneric>) => {
@@ -46,6 +39,10 @@ const createWebComponentBaseClass = <StateGeneric>(
       this.attachShadow({ mode: "open" })
 
       this.template.subscribe(() => {
+        this.render()
+      })
+
+      this.state.listen(() => {
         this.render()
       })
     }
